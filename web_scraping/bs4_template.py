c3289030d579d50b28b385241b320ff073bcdd22
Jared Elliott <jwelliott28@gmail.com>
2022-09-15 11:01:40 -0400

    added basic script for scraping a team page for WRT and QB. Script finds table element, but does not scrape data and leaves result set blank.

---
+++ web_scraping/bs4_template.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests, bs4
+
+#baseUrl = 'https://pro-football-reference.com/teams'
+
+#Download the desired webpage - use ARZ 'crd' as a test for now
+response = requests.get('https://pro-football-reference.com/teams/crd/2022.htm')
+
+#Condition that kills program if status code is !200
+response.raise_for_status()
+
+#Create a bs4 object from the downloaded webpage
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+#Scrape the CSV file for W/R/T and QB
+
+#Right now this recognizes the table and picks it up as a bs4 result set, but does not scrape any data. Returns a length of 0.
+wrt_csv = soup.select('table #rushing_and_receiving')
+print(type(wrt_csv))
+
+#qb_csv = soup.select('TO_DO: get csv table for qb attribute')
+
+
+#Verify the csv was correctly scraped
+print(str(wrt_csv))
+print(str(qb_csv))
